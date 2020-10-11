b694f9afad46c9b1e626a0e2e8f02d6be18c96da
elena <elena@carts.guru>
2020-10-11 12:40:37 +0200

    Create project structure with create-react-library and add components folder to start creating  components well structured with CSS

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { ExampleComponent, MainButton } from 'ruca-react-ui'
+import 'ruca-react-ui/dist/index.css'
+
+const App = () => {
+  return (
+    <div>
+       <MainButton text="Hola" />
+    </div>
+  )
+}
+
+export default App
